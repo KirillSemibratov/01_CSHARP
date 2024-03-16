7fbecde9c223abaa216e485d5a55aaf7c2427e35
Kirill Semibratov <KSemibratoFF@gmail.com>
2024-03-16 20:56:06 +0300

    add lesson1:task6

---
+++ lesson1/Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 3;
+int b = 18;
+int c = 15;
+int d = 47;
+
+double product1 = a * b;
+Console.WriteLine(product1);
+
+int sum = c + d;
+Console.WriteLine(sum);
+
+double quotient = product1 / sum;
+Console.WriteLine(quotient);
